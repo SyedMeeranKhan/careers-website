2859433e655737e0b9e55f4267bb7c9eda5c7d89
Meeran <meerankham@gmail.com>
2024-08-09 18:27:42 +0000

    Basic structure complete

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS =[
+  {
+    'id':1,
+    'title': "frontend-dev",
+    'location': 'Islamabad,Pakistan',
+    'salary': 'Rs. 2000000'
+  },
+  {
+    'id':2,
+    'title': "backend-dev",
+    'location': 'Karachi,Pakistan',
+    'salary': 'Rs. 1500000'
+  },
+  {
+    'id':3,
+    'title': "fullstack-dev",
+    'location': 'Lahore,Pakistan'
+    
+  },
+  {
+    'id':4,
+    'title': "flutter-dev",
+    'location': 'remote',
+    'salary': 'Rs. 1200000'
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',jobs=JOBS)
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 # print(__name__)
 if __name__ == '__main__':
